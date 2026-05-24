ceda00203d7a370a0817f78d3e9fa23ef83bfb05
Kaosi Anikwe <anikwehenryasa@gmail.com>
2026-05-24 07:44:30 +0100

    refactor: remove unused dependencies and delete Sanity configuration file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
-import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "6o5sii6d",
-      dataset: "production",
-      studioBasePath: "/studio",
-      useCdn: true,
-      apiVersion: "2024-01-01",
-    }),
-    react(),
-  ],
   vite: {
     plugins: [tailwindcss()],
   },
